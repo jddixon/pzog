c17c3df63e3940ce325aa37043b2ce942e0443aa
Jim Dixon <jddixon@gmail.com>
2017-12-13 14:39:39 -0800

    pylinting, bug fixes

--- src/pzog/__init__.py
+++ src/pzog/__init__.py
@@ -1,7 +1,7 @@
 # ~/dev/py/pzog/pzog/__init__.py
 
-__version__ = '0.6.10'
-__version_date__ = '2017-09-29'
+__version__ = '0.6.11'
+__version_date__ = '2017-12-13'
 
 __all__ = ['__version__', '__version_date__',
            'PZOG_MAX_MSG', 'PZOG_PORT', 'RING_IP_ADDR',
