9536c86406780a78778da40fd8de76d96935ebab
Jim Dixon <jddixon@gmail.com>
2018-03-23 12:43:47 -0700

    fix corrupt .dvcz/builds

--- setup.py
+++ setup.py
@@ -6,22 +6,23 @@
 from os.path import exists
 from setuptools import setup
 
-LONG_DESC = None
+long_desc = None
 if exists('README.md'):
     with open('README.md', 'r') as file:
-        LONG_DESC = file.read()
+        long_desc = file.read()
 
 setup(name='pzog',
-      version='0.6.12',
+      version='0.6.13',
       author='Jim Dixon',
       author_email='jddixon@gmail.com',
-      long_description=LONG_DESC,
+      long_description=long_desc,
       packages=['pzog'],
       package_dir={'': 'src'},
       py_modules=[],
       include_package_data=False,
       zip_safe=False,
       scripts=['src/psprog', 'src/pzogd', 'src/ring_data_gen'],
+      ext_modules=[],
       description='organize ring of 5-6 machines sharing data over full mesh',
       url='https://jddixon.github.io/pzog',
       classifiers=[
@@ -29,7 +30,10 @@ setup(name='pzog',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: MIT License',
           'Natural Language :: English',
+          'Programming Language :: Python',
+          'Programming Language :: Python 2',
           'Programming Language :: Python 2.7',
+          'Programming Language :: Python 3',
           'Programming Language :: Python 3.5',
           'Programming Language :: Python 3.6',
           'Programming Language :: Python 3.7',
